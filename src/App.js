5c678ed99c7b6cd1c6800f1573a996f24d7ebb4f
Timothy-Achonu <achonutimothy@gmail.com>
2022-10-27 15:13:05 +0100

    adding the subtext element for each link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import imgSrc from "./assets/profile__img.png";
+import imgSrc from "./assets/tim.jpg";
 
 function App() {
   
@@ -9,28 +9,34 @@ function App() {
         <figure>
           <img id="profile__img" src={imgSrc} alt="user-profile-img"/>
         </figure>
-        <div id="twitter">Annetta Black</div>
+        <div id="twitter">Timothy Achonu</div>
         <div id="slack">TIMOTHY Achonu</div>
         <div className="three-dots">...</div>
       </div>
       <div className="links-container">
       <a href="https://twitter.com/TimothyAchonu" id="twitter-link">
           Twitter
+          <span className="display-none">TimothyAchonu</span>
         </a>
         <a href="https://training.zuri.team/" id="btn__zuri">
           Zuri-Team
+          <span className="display-none">TimothyAchonu</span>
         </a>
         <a href="http://books.zuri.team" id="books">
           Books
+          <span className="display-none">TimothyAchonu</span>
         </a>
         <a href="https://books.zuri.team/python-for-beginners?ref_id=TIMOTHY Achonu" id="book__python">
           Python Books
+          <span className="display-none">TimothyAchonu</span>
         </a>
         <a href="https://training.zuri.team/" id="pitch">
           Background Check for Coders
+          <span className="display-none">TimothyAchonu</span>
         </a>
         <a href="https://books.zuri.team/design-rules" id="book__design">
            Design Books
+           <span className="display-none">TimothyAchonu</span>
         </a>
       </div>
     </div>
