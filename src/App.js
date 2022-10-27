3cafb2319bd756ae8f18f933c1cb47111ee7994c
Timothy-Achonu <achonutimothy@gmail.com>
2022-10-27 11:17:49 +0100

    adding the profile image and a tags with some basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import './App.css';
+import "./App.css";
+import imgSrc from "./assets/profile__img.png";
 
 function App() {
+  
   return (
     <div className="App">
-       <h1>HNG BEGINS</h1>
+      <div className="person-profile">
+        <figure>
+          <img id="profile__img" src={imgSrc} alt="user-profile-img"/>
+        </figure>
+        <div id="twitter">Timothy Achonu</div>
+        <div id="slack">TIMOTHY Achonu</div>
+        <div className="three-dots">...</div>
+      </div>
+      <div className="links-container">
+      <a href="https://twitter.com/TimothyAchonu" id="twitter-link">
+          Twitter
+        </a>
+        <a href="https://training.zuri.team/" id="btn__zuri">
+          Zuri-Team
+        </a>
+        <a href="http://books.zuri.team" id="books">
+          Books
+        </a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>," id="book__python">
+          Python Books
+        </a>
+        <a href="https://training.zuri.team/" id="pitch">
+          Background Check for Coders
+        </a>
+        <a href="https://books.zuri.team/design-rules" id="book__design">
+           Design Books
+        </a>
+      </div>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Links() {
+  return (
+    <a 
+     href='https://training.zuri.team/' id="btn__zuri" >
+        Zuri-Team
+    </a>
+  )
+}
